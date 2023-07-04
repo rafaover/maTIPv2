d3d4112c23105badcfb8c1669da6dcd57a0b1a18
rafaover <rafamsilva@gmail.com>
2023-07-04 10:27:32 +1000

    Started project. Android 24.

---
+++ app/src/main/java/com/exercise/matipv2/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.exercise.matipv2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.exercise.matipv2.ui.theme.MaTIPv2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaTIPv2Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                }
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
