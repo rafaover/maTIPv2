184f7f865fa762f8eb50325bf26632b74b9a8988
rafaover <rafamsilva@gmail.com>
2024-04-23 12:04:45 +1000

    .dev-viewmodel - called viewmodel functions to get value for composables. - created viewmodel instance (if necessary this will be moved to a factory) - created uiState instance.

--- app/src/main/java/com/exercise/matipv2/ui/MainLayout.kt
+++ app/src/main/java/com/exercise/matipv2/ui/MainLayout.kt
@@ -11,10 +11,8 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -22,21 +20,17 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.exercise.matipv2.R
 import com.exercise.matipv2.components.EditNumber
 import com.exercise.matipv2.components.RoundTheTipSwitch
-import com.exercise.matipv2.util.calculateTip
 
 @SuppressLint("VisibleForTests")
 @Composable
 fun MainLayout() {
-    /* hoisting the state for EditNumber func */
-    var amountInput by rememberSaveable { mutableStateOf("") }
-    val amount = amountInput.toDoubleOrNull() ?: 0.0
-    var tipPercentInput by rememberSaveable { mutableStateOf("") }
-    val tipPercent = tipPercentInput.toDoubleOrNull() ?: 0.0
-    var roundUp by rememberSaveable { mutableStateOf(false) }
-    val tip = calculateTip(amount, tipPercent, roundUp)
+
+    val viewModel: MainLayoutViewModel = viewModel()
+    val uiState by viewModel.uiState.collectAsState()
 
     Column(
         modifier = Modifier
@@ -63,8 +57,8 @@ fun MainLayout() {
                 .padding(bottom = 15.dp)
                 .align(alignment = Alignment.Start),
             label = R.string.bill_amount,
-            value = amountInput,
-            onValueChange = { amountInput = it },
+            value = uiState.amountInput,
+            onValueChange = { viewModel.updateAmountInput(it) },
             keyboardOptions = KeyboardOptions(
                 keyboardType = KeyboardType.Number,
                 imeAction = ImeAction.Next
@@ -78,22 +72,25 @@ fun MainLayout() {
                 .padding(bottom = 40.dp)
                 .align(alignment = Alignment.Start),
             label = R.string.tip_percentage,
-            value = tipPercentInput,
-            onValueChange = { tipPercentInput = it },
+            value = uiState.tipPercentInput,
+            onValueChange = { viewModel.updateTipPercentInput(it) },
             keyboardOptions = KeyboardOptions(
                 keyboardType = KeyboardType.Number,
                 imeAction = ImeAction.Done
             ),
             leadingIcon = R.drawable.percent
         )
-        RoundTheTipSwitch(roundUp, onRoundUpChange = { roundUp = it })
+        RoundTheTipSwitch(
+            roundUp = uiState.roundUp,
+            onRoundUpChange = { viewModel.updateRoundUp(it) }
+        )
 
         /* Text Box for total Tip Amount */
         Text(
             modifier = Modifier
                 .align(alignment = Alignment.Start)
                 .padding(bottom = 16.dp),
-            text = stringResource(R.string.tip_amount, tip),
+            text = stringResource(R.string.tip_amount, viewModel.finalTip()),
             style = MaterialTheme.typography.displaySmall,
         )
 
