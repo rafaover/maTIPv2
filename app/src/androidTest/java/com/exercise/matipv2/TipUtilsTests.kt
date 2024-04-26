d93c7760eb36ff59e43122e28f7060db18ce63ae
rafaover <rafamsilva@gmail.com>
2024-04-26 12:48:20 +1000

    Testing development for Utils functions

---
+++ app/src/androidTest/java/com/exercise/matipv2/TipUtilsTests.kt
@@ -0,0 +1,46 @@
+package com.exercise.matipv2
+
+import androidx.lifecycle.ViewModel
+import com.exercise.matipv2.ui.MainLayoutViewModel
+import com.exercise.matipv2.util.calculateTip
+import com.exercise.matipv2.util.splitTipValue
+import com.exercise.matipv2.util.stringAmountToDouble
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class TipUtilsTests() {
+    @Test
+    fun testStringAmountToDouble() {
+        val amount = "100"
+        val expected = 100.0
+        val actual = stringAmountToDouble(amount)
+        assertEquals(expected, actual, 0.0)
+    }
+
+    @Test
+    fun testCalculateTip() {
+        val amount = 100.0
+        val tipPercent = 15.0
+        val roundUp = false
+        val expected = "$15.00"
+        val actual = calculateTip(amount, tipPercent, roundUp)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testSplitTipValue() {
+        val tipAmount = "15.0"
+        val splitShare = "2"
+        val expected = 7.5
+        val actual = splitTipValue(tipAmount, splitShare)
+        assertEquals(expected, actual, 0.0)
+    }
+
+    @Test
+    fun testFinalTip() {
+        val amount = "100"
+        val tipPercent = "15"
+        val roundUp = false
+        val expected = "$15.00"
+    }
+}
