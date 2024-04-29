129a632639da348dfc1efc5132952f0d39a3edb2
rafaover <rafamsilva@gmail.com>
2024-04-29 14:53:09 +1000

    minor primary colour update

--- app/src/main/java/com/exercise/matipv2/ui/theme/Color.kt
+++ app/src/main/java/com/exercise/matipv2/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.exercise.matipv2.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val md_theme_light_primary = Color(0xFF833AB6)
+val md_theme_light_primary = Color(0xFF5A177A)
 val md_theme_light_onPrimary = Color(0xFFFFFFFF)
 val md_theme_light_primaryContainer = Color(0xFFF3DAFF)
 val md_theme_light_onPrimaryContainer = Color(0xFF2F004C)
