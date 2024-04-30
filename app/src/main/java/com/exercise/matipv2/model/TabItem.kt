2e9284d2a44e37200b5c3c22b3b9cbec0cdeb995
rafaover <rafamsilva@gmail.com>
2024-04-30 14:26:21 +1000

    .feature-TopTabRow Data Class(model) for TabItem

---
+++ app/src/main/java/com/exercise/matipv2/model/TabItem.kt
@@ -0,0 +1,9 @@
+package com.exercise.matipv2.model
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class TabItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector
+)
