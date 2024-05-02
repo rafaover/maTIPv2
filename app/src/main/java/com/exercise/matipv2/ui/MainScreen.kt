c72ab6193ca82ceb6acfe3ae90e3d55d19f48a3f
rafaover <rafamsilva@gmail.com>
2024-05-02 10:13:38 +1000

    .Impl-Nav-TopRowTab Moved TipCalculatorScreen to own file

--- app/src/main/java/com/exercise/matipv2/ui/MainScreen.kt
+++ app/src/main/java/com/exercise/matipv2/ui/MainScreen.kt
@@ -1,34 +1,14 @@
 package com.exercise.matipv2.ui
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.exercise.matipv2.R
-import com.exercise.matipv2.components.calculator.SplitCounter
-import com.exercise.matipv2.components.calculator.TotalTipAmount
-import com.exercise.matipv2.components.common.EditNumber
-import com.exercise.matipv2.components.common.RoundTheTipSwitch
 import com.exercise.matipv2.components.common.TopTabRow
+import com.exercise.matipv2.ui.tipcalculator.TipCalculatorScreen
 
 @SuppressLint("VisibleForTests")
 @Composable
@@ -40,71 +20,10 @@ fun MainScreen(viewModel: MainScreenViewModel = viewModel()) {
             currentTabIndex = uiState.selectedTabIndex,
             selectedTabIndex = { viewModel.updateSelectedTabIndex(it) }
         )
-
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(40.dp)
-                .verticalScroll(rememberScrollState()),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Top,
-        ) {
-            /* Title */
-            Text(
-                modifier = Modifier
-                    .padding(bottom = dimensionResource(R.dimen.padding_mid))
-                    .align(alignment = Alignment.Start),
-                text = stringResource(R.string.calculate_tip),
-                fontWeight = FontWeight.Bold,
-                style = MaterialTheme.typography.titleLarge,
-            )
-
-            /* Edit Total Bill Amount */
-            EditNumber(
-                modifier = Modifier
-                    .padding(bottom = dimensionResource(R.dimen.padding_mid))
-                    .align(alignment = Alignment.Start),
-                label = R.string.bill_amount,
-                value = uiState.amountInput,
-                onValueChange = { viewModel.updateAmountInput(it) },
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Number,
-                    imeAction = ImeAction.Next
-                ),
-                leadingIcon = R.drawable.attach_money
-            )
-
-            /* Edit Tip percentage */
-            EditNumber(
-                modifier = Modifier
-                    .padding(bottom = 40.dp)
-                    .align(alignment = Alignment.Start),
-                label = R.string.tip_percentage,
-                value = uiState.tipPercentInput,
-                onValueChange = { viewModel.updateTipPercentInput(it) },
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Number,
-                    imeAction = ImeAction.Done
-                ),
-                leadingIcon = R.drawable.percent
-            )
-
-            SplitCounter(
-                splitUiState = uiState.splitShare,
-                onclickPositive = { viewModel.increaseCounter() },
-                onclickNegative = { viewModel.decreaseCounter() }
-            )
-
-            TotalTipAmount(viewModel.finalTip())
-
-            RoundTheTipSwitch(
-                modifier = Modifier
-                    .padding(bottom = dimensionResource(R.dimen.padding_mid))
-                    .align(alignment = Alignment.Start),
-                roundUp = uiState.roundUp,
-                onRoundUpChange = { viewModel.updateRoundUp(it) }
-            )
-        }
+        TipCalculatorScreen(
+            viewModel = viewModel,
+            uiState = uiState
+        )
     }
 }
 
---
+++ app/src/main/java/com/exercise/matipv2/ui/tipcalculator/TipCalculatorScreen.kt
@@ -0,0 +1,98 @@
+package com.exercise.matipv2.ui.tipcalculator
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import com.exercise.matipv2.R
+import com.exercise.matipv2.components.calculator.SplitCounter
+import com.exercise.matipv2.components.calculator.TotalTipAmount
+import com.exercise.matipv2.components.common.EditNumber
+import com.exercise.matipv2.components.common.RoundTheTipSwitch
+import com.exercise.matipv2.model.MainScreenState
+import com.exercise.matipv2.ui.MainScreenViewModel
+
+@Composable
+fun TipCalculatorScreen(
+    viewModel: MainScreenViewModel,
+    uiState: MainScreenState,
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(40.dp)
+            .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+    ) {
+        /* Title */
+        Text(
+            modifier = Modifier
+                .padding(bottom = dimensionResource(R.dimen.padding_mid))
+                .align(alignment = Alignment.Start),
+            text = stringResource(R.string.calculate_tip),
+            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.titleLarge,
+        )
+
+        /* Edit Total Bill Amount */
+        EditNumber(
+            modifier = Modifier
+                .padding(bottom = dimensionResource(R.dimen.padding_mid))
+                .align(alignment = Alignment.Start),
+            label = R.string.bill_amount,
+            value = uiState.amountInput,
+            onValueChange = { viewModel.updateAmountInput(it) },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            leadingIcon = R.drawable.attach_money
+        )
+
+        /* Edit Tip percentage */
+        EditNumber(
+            modifier = Modifier
+                .padding(bottom = 40.dp)
+                .align(alignment = Alignment.Start),
+            label = R.string.tip_percentage,
+            value = uiState.tipPercentInput,
+            onValueChange = { viewModel.updateTipPercentInput(it) },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            leadingIcon = R.drawable.percent
+        )
+
+        SplitCounter(
+            splitUiState = uiState.splitShare,
+            onclickPositive = { viewModel.increaseCounter() },
+            onclickNegative = { viewModel.decreaseCounter() }
+        )
+
+        TotalTipAmount(viewModel.finalTip())
+
+        RoundTheTipSwitch(
+            modifier = Modifier
+                .padding(bottom = dimensionResource(R.dimen.padding_mid))
+                .align(alignment = Alignment.Start),
+            roundUp = uiState.roundUp,
+            onRoundUpChange = { viewModel.updateRoundUp(it) }
+        )
+    }
+}
