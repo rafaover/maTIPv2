a8edf4414f12b1d1db2e8e9e0aad57839aa60fed
rafaover <rafamsilva@gmail.com>
2024-05-04 11:04:37 +1000

    .feature-roomdb Created TIP model and EVENT model

---
+++ app/src/main/java/com/exercise/matipv2/data/model/Event.kt
@@ -0,0 +1,7 @@
+package com.exercise.matipv2.data.model
+
+data class Event(
+    val id: Int,
+    val name: String,
+    val tipList: List<Tip>
+)
---
+++ app/src/main/java/com/exercise/matipv2/data/model/Tip.kt
@@ -0,0 +1,8 @@
+package com.exercise.matipv2.data.model
+
+data class Tip(
+    val id: Int,
+    val tipAmount: String,
+    val tipPercent: String,
+    val tipDate: String
+)
