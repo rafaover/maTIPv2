898986b31bdeb7d3e47f65e5d1766b81e230da94
rafaover <rafamsilva@gmail.com>
2024-05-04 11:18:24 +1000

    .feature-roomdb Configured entities on Models: Tips and Events tables.

--- app/src/main/java/com/exercise/matipv2/data/model/Event.kt
+++ app/src/main/java/com/exercise/matipv2/data/model/Event.kt
@@ -1,7 +1,15 @@
 package com.exercise.matipv2.data.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "events")
 data class Event(
+    @PrimaryKey(autoGenerate = true)
     val id: Int,
+    @ColumnInfo(name = "event_name")
     val name: String,
+    @ColumnInfo(name = "tip_list")
     val tipList: List<Tip>
 )
--- app/src/main/java/com/exercise/matipv2/data/model/Tip.kt
+++ app/src/main/java/com/exercise/matipv2/data/model/Tip.kt
@@ -1,8 +1,17 @@
 package com.exercise.matipv2.data.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tips")
 data class Tip(
+    @PrimaryKey(autoGenerate = true)
     val id: Int,
+    @ColumnInfo(name = "tip_amount")
     val tipAmount: String,
+    @ColumnInfo(name = "tip_percent")
     val tipPercent: String,
+    @ColumnInfo(name = "tip_date")
     val tipDate: String
 )
