abd3e16076118e82d8719fc844ae1612c5db5ffd
rafaover <rafamsilva@gmail.com>
2024-05-09 17:35:12 +1000

    .DI-impl-hilt Added a callback with it's function to populate the db with a few info. Still not creating the DB. :(

--- app/src/main/java/com/exercise/matipv2/di/DatabaseModule.kt
+++ app/src/main/java/com/exercise/matipv2/di/DatabaseModule.kt
@@ -1,17 +1,25 @@
 package com.exercise.matipv2.di
 
 import android.content.Context
+import android.util.Log
 import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.exercise.matipv2.data.EventDao
 import com.exercise.matipv2.data.MatipDatabase
 import com.exercise.matipv2.data.MatipRepository
 import com.exercise.matipv2.data.OfflineMatipRepository
 import com.exercise.matipv2.data.TipDao
+import com.exercise.matipv2.data.model.Event
+import com.exercise.matipv2.data.model.Tip
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Singleton
 
 @Module
@@ -20,10 +28,34 @@ object DatabaseModule {
     @Singleton
     @Provides
     fun getDatabase(@ApplicationContext context: Context): MatipDatabase {
-        return Room.databaseBuilder(context, MatipDatabase::class.java, "app_database")
+        lateinit var database: MatipDatabase
+        database = Room.databaseBuilder(context, MatipDatabase::class.java, "app_database")
                 .fallbackToDestructiveMigration()
+                .addCallback(object : RoomDatabase.Callback() {
+                    override fun onCreate(db: SupportSQLiteDatabase) {
+                        super.onCreate(db)
+                        CoroutineScope(Dispatchers.IO).launch {
+                            prePopulateDb(
+                                database.eventDao(),
+                                database.tipDao()
+                            )
+                        }
+                    }
+                })
                 .build()
+        Log.d("DatabaseModule","Database has been created")
+        return database
     }
+
+    suspend fun prePopulateDb(eventDao: EventDao, tipDao: TipDao) {
+        val event = Event(1, "Event 1")
+        eventDao.insert(event)
+        val tip1 = Tip(1, "100", "10.0", 1)
+        val tip2 = Tip(2, "200", "20.0", 1)
+        tipDao.insertTip(tip1)
+        tipDao.insertTip(tip2)
+    }
+
     @Provides
     fun getEventDao(database: MatipDatabase) = database.eventDao()
     @Provides
