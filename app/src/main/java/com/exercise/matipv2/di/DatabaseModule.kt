e4f52dac107f6306740c00682732d60814738d7f
rafaover <rafamsilva@gmail.com>
2024-05-10 10:26:18 +1000

    .DI-impl-hilt Hilt Modules - Refactored DatabaseModule - Removed Repository from this module, created it's own module (RepositoryModule with binding)

--- app/src/main/java/com/exercise/matipv2/di/DatabaseModule.kt
+++ app/src/main/java/com/exercise/matipv2/di/DatabaseModule.kt
@@ -1,13 +1,8 @@
 package com.exercise.matipv2.di
 
 import android.content.Context
-import android.util.Log
 import androidx.room.Room
-import com.exercise.matipv2.data.dao.EventDao
 import com.exercise.matipv2.data.MatipDatabase
-import com.exercise.matipv2.data.MatipRepository
-import com.exercise.matipv2.data.OfflineMatipRepository
-import com.exercise.matipv2.data.dao.TipDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -21,19 +16,17 @@ object DatabaseModule {
     @Singleton
     @Provides
     fun getDatabase(@ApplicationContext context: Context): MatipDatabase {
-        val database = Room.databaseBuilder(context, MatipDatabase::class.java, "app_database")
-                .fallbackToDestructiveMigration()
-                .build()
-        Log.d("DatabaseModule","Database has been created")
-        return database
+        return Room.databaseBuilder(
+            context,
+            MatipDatabase::class.java,
+            "matip_database"
+        )
+            .fallbackToDestructiveMigration()
+            .build()
     }
 
     @Provides
     fun getEventDao(database: MatipDatabase) = database.eventDao()
     @Provides
     fun getTipDao(database: MatipDatabase) = database.tipDao()
-    @Provides
-    fun provideMatipRepository(eventDao: EventDao, tipDao: TipDao): MatipRepository {
-        return OfflineMatipRepository(tipDao, eventDao)
-    }
 }
---
+++ app/src/main/java/com/exercise/matipv2/di/RepositoryModule.kt
@@ -0,0 +1,17 @@
+package com.exercise.matipv2.di
+
+import com.exercise.matipv2.data.MatipRepository
+import com.exercise.matipv2.data.OfflineMatipRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+    @Binds
+    abstract fun bindOfflineMatipRepository(
+        offlineMatipRepository: OfflineMatipRepository
+    ): MatipRepository
+}
