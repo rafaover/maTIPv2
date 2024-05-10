3b791f06c333a83f142fbd6c2cbfaf17593b88a7
rafaover <rafamsilva@gmail.com>
2024-05-10 10:32:03 +1000

    .DI-impl-hilt Removed unused imports.

--- app/src/main/java/com/exercise/matipv2/MainActivity.kt
+++ app/src/main/java/com/exercise/matipv2/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.exercise.matipv2.ui.MainScreen
 import com.exercise.matipv2.ui.MainScreenViewModel
 import com.exercise.matipv2.ui.theme.MaTIPv2Theme
