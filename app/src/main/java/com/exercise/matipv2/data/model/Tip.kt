315dece9cc47fabec8f47c5ce868369c65b9e73c
rafaover <rafamsilva@gmail.com>
2024-05-15 11:47:59 +1000

    .32-dev-events-page Minor Tip Model update.

--- app/src/main/java/com/exercise/matipv2/data/model/Tip.kt
+++ app/src/main/java/com/exercise/matipv2/data/model/Tip.kt
@@ -7,11 +7,11 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "tips")
 data class Tip(
     @PrimaryKey(autoGenerate = true)
-    val id: Int,
+    val id: Int = 0,
     @ColumnInfo(name = "tip_amount")
     val tipAmount: String,
     @ColumnInfo(name = "tip_percent")
     val tipPercent: String,
     @ColumnInfo(name = "event_id")
-    val eventId: Int
+    var eventId: Int? = null
 )
