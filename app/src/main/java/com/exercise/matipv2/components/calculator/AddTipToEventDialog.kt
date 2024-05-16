a5f12cb1773d18ed055f2cad02ddb3b8cd400725
rafaover <rafamsilva@gmail.com>
2024-05-16 13:33:09 +1000

    .41-add-floating-button-+event-in-the-events-page - Updated AddTipToEventDialogBox. - Repositioned save button, changed arrangement and paddings. - Renamed parameters to maintain order of data.

--- app/src/main/java/com/exercise/matipv2/components/calculator/AddTipToEventDialog.kt
+++ app/src/main/java/com/exercise/matipv2/components/calculator/AddTipToEventDialog.kt
@@ -3,7 +3,6 @@ package com.exercise.matipv2.components.calculator
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
@@ -11,11 +10,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.selection.selectable
-import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -29,44 +28,58 @@ import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
-import androidx.compose.ui.window.DialogProperties
 import com.exercise.matipv2.R
 import com.exercise.matipv2.data.model.Event
+import com.exercise.matipv2.ui.MainScreenViewModel
 import kotlinx.coroutines.flow.Flow
 
 @Composable
 fun AddTipToEventDialogBox(
-    onDismissRequest: () -> Unit,
+    viewModel: MainScreenViewModel,
     allEvents: Flow<List<Event>>,
     onEventSelected: (Event) -> Unit
 ) {
     val allEventsFlow by allEvents.collectAsState(initial = emptyList())
+    val (selectedOption, onOptionSelected) =
+        remember { mutableStateOf<Event?>(null) }
 
     Dialog(
-        onDismissRequest = { onDismissRequest() },
-        properties = DialogProperties(dismissOnBackPress = true)
+        onDismissRequest = { viewModel.updateShowDialog(false) }
     ) {
         Card(
             modifier = Modifier
                 .fillMaxWidth()
-                .heightIn(min = 200.dp, max = 500.dp)
-                .padding(dimensionResource(R.dimen.padding_mid))
+                .heightIn(min = 280.dp, max = 380.dp)
         ) {
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(dimensionResource(R.dimen.padding_lg)),
-                verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_sml))
+                    .padding(dimensionResource(R.dimen.padding_dialog)),
+                verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_mid))
             ) {
-                Text(
-                    text = stringResource(R.string.event_dialog_title),
-                    style = MaterialTheme.typography.titleLarge,
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier.align(Alignment.CenterHorizontally)
-                )
-
-                val (selectedOption, onOptionSelected) =
-                    remember { mutableStateOf<Event?>(null) }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = stringResource(R.string.select_list),
+                        style = MaterialTheme.typography.titleLarge,
+                        fontWeight = FontWeight.Bold,
+                    )
+                    /* Button to Save the selected event */
+                    TextButton(
+                        enabled = selectedOption != null,
+                        onClick = {
+                            selectedOption?.let { onEventSelected(it) }
+                            viewModel.updateShowDialog(false)
+                        },
+                        modifier = Modifier
+                            .padding(dimensionResource(R.dimen.padding_button))
+                    ) {
+                        Text(stringResource(R.string.confirm))
+                    }
+                }
                 LazyColumn(
                     modifier = Modifier.weight(1f),
                     userScrollEnabled = true
@@ -95,16 +108,6 @@ fun AddTipToEventDialogBox(
                             )
                         }
                     }
-                }
-                Spacer(modifier = Modifier.weight(1f))
-                Button(
-                    enabled = selectedOption != null,
-                    onClick = {
-                        selectedOption?.let { onEventSelected(it) }
-                        onDismissRequest()
-                    }
-                ) {
-                    Text(stringResource(R.string.confirm))
                 }
             }
         }
