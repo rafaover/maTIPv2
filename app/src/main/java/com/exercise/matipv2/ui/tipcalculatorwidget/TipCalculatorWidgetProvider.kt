50588e887674f3f27da2efb90cd210b6deb32fb4
rafaover <rafamsilva@gmail.com>
2024-07-11 12:33:27 +1000

    .92-impl-jetpack-glance updates

--- app/src/main/java/com/exercise/matipv2/ui/tipcalculatorwidget/TipCalculatorWidgetProvider.kt
+++ app/src/main/java/com/exercise/matipv2/ui/tipcalculatorwidget/TipCalculatorWidgetProvider.kt
@@ -1,12 +1,14 @@
 package com.exercise.matipv2.ui.tipcalculatorwidget
 
 import android.content.Context
+import androidx.compose.runtime.Composable
 import androidx.glance.GlanceId
 import androidx.glance.GlanceTheme
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import androidx.glance.appwidget.provideContent
 import androidx.glance.text.Text
+import com.exercise.matipv2.data.local.model.Tip
 
 class TipCalculatorWidgetReceiver: GlanceAppWidgetReceiver() {
 
@@ -19,8 +21,13 @@ class TipCalculatorWidget: GlanceAppWidget() {
     override suspend fun provideGlance(context: Context, id: GlanceId) {
         provideContent {
             GlanceTheme {
-                Text(text = "Hello World!")
+                TipCalculatorWidgetScreen()
             }
         }
     }
+}
+
+@Composable
+fun TipCalculatorWidgetScreen() {
+    Text(text = "Hello World!")
 }
