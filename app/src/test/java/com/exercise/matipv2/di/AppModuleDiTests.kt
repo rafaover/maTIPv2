0fda463375695ed25314116597b667d8eddf6acb
rafaover <rafamsilva@gmail.com>
2024-07-17 11:56:05 +1000

    .53-build-tests Added Koin modules verification - updated dependencies file

---
+++ app/src/test/java/com/exercise/matipv2/di/AppModuleDiTests.kt
@@ -0,0 +1,15 @@
+package com.exercise.matipv2.di
+
+import org.junit.Test
+import org.koin.core.annotation.KoinExperimentalAPI
+import org.koin.test.KoinTest
+import org.koin.test.verify.verify
+
+class CheckModulesTestL: KoinTest {
+
+    @OptIn(KoinExperimentalAPI::class)
+    @Test
+    fun checkAllModules() {
+        appModule.verify()
+    }
+}
